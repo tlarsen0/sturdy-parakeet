016c4572262b1e78037648157ab52910d58951fd
tlarsen0 <72167435+tlarsen0@users.noreply.github.com>
2025-06-22 14:52:52 -0500

    Small formatting fix.

--- parakeet/small_parakeet.py
+++ parakeet/small_parakeet.py
@@ -3,7 +3,7 @@ client = OpenAI()
 
 response = client.responses.create(
     model = "gpt-4.1",
-    input="Write a one-sentence story about two dogs and a plushy squid toy.",
+    input = "Write a one-sentence story about two dogs and a plushy squid toy.",
 )
 
 print(response.output_text)
