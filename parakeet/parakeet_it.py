5b670447ddeccf2bf7dd80dad0d251fb6156f268
tlarsen0 <72167435+tlarsen0@users.noreply.github.com>
2025-06-23 07:52:08 -0500

    Convert to modular call. Added code to support convert to C# and Python.

--- parakeet/parakeet_it.py
+++ parakeet/parakeet_it.py
@@ -16,11 +16,12 @@ def summarize_code(file_name):
         print("Parakeet says:")
         print(response.output_text)
 
-def convert_code_java(file_name):
-    print(f'Convert code to Java: {file_name}')
+
+def convert_code_helper(file_name, new_language_pretty):
+    print(f'Convert code to {new_language_pretty}: {file_name}')
     with open(file_name, 'r') as code_file:
         code = code_file.read()
-        prompt = f"Convert the following code into Java:\n ```{code}```"
+        prompt = f"Convert the following code into {new_language_pretty}:\n ```{code}```"
         ##print(f"prompt = {prompt}")
         response = client.responses.create(
             model = 'gpt-4.1',
@@ -29,15 +30,14 @@ def convert_code_java(file_name):
         print("Parakeet says:")
         print(response.output_text)
 
-def convert_code_csharp(file_name):
-    print(f'Convert code to C#: {file_name}')
-
-def convert_code_python(file_name):
-    print(f'Convert code to Python: {file_name}')
 
 
 def suggest_unit_tests(file_name):
     print(f'Suggest unit tests: {file_name}')
+    with open(file_name, 'r') as code_file:
+        code = code_file.read()
+        prompt = f"Suggest unit tests for the following code:\n ```{code}```"
+        print(f"prompt = {prompt}")
 
 
 print("parakeet v0.1: for fun and learning.")
@@ -49,7 +49,14 @@ theCommand = sys.argv[1]
 theFile = sys.argv[2]
 if theCommand == 'summarize':
     summarize_code(theFile)
-elif theCommand == 'covert-java':
-    convert_code_java(theFile)
+elif theCommand == 'convert-java':
+    convert_code_helper(theFile, 'Java')
+elif theCommand == 'convert-csharp':
+    convert_code_helper(theFile, 'C#')
+elif theCommand == 'convert-python':
+    convert_code_helper(theFile, 'Python')
 elif theCommand == 'unit-tests':
     suggest_unit_tests(theFile)
+else:
+    print(f"Unknown command: {theCommand}")
+    sys.exit(1)
